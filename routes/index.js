0337158d248ee5c48f8afff7f37f704f5a7f9745
yubi00 <ubrajkhadka@gmail.com>
2018-09-20 13:05:40 +1000

    code refactored

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,20 @@
-var request = require('request');
 var url = require('url');
+var axios = require('axios');
 
-
-var movies = [];
+var movies_fw = [];
+var movies_cw = [];
 var single_movie = [];
 
-var options = {
+
+var options_fw = {
+    url: 'http://webjetapitest.azurewebsites.net/api/filmworld/movies',
+    method: 'GET',
+    headers: {
+        'x-access-token':'sjd1HfkjU83ksdsm3802k'
+    }
+}
+
+var options_cw = {
     url: 'http://webjetapitest.azurewebsites.net/api/cinemaworld/movies',
     method: 'GET',
     headers: {
@@ -15,56 +24,72 @@ var options = {
 
 
 //home route
-exports.home =  function(req, res){
-    request(options, function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            movies = JSON.parse(body);
-           res.render('index', {
-               title: "Star Wars Movies",
-               movies: movies
-           });
-          }
-    });
+exports.home =  async (req, res) => {
+    try {
+        let fw_response = await axios(options_fw);
+        let cw_response = await axios(options_cw);
+        movies_fw = fw_response.data;
+        movies_cw = cw_response.data; 
+        res.render('index', {
+            title: "Star Wars Movies",
+            movies_fw: movies_fw,
+            movies_cw: movies_cw
+        });
+    }
+    catch(e) {
+        console.log("Error: "+e);
+    }
+          
 };
 
 //for single episode
-exports.single_movie = function(req, res){
+exports.single_movie = async (req, res) => {
     
-    var movie_title = req.query.movie_title;
-    var movie = '';
-    var movieid = '';
-    var price_cw = ''; 
+    movie_title = req.query.movie_title;
+    var fw_movieid = '';
+    var cw_movieid = '';
+    var price_fw = 0; 
+    var price_cw = 0;
+    var options = '';
 
-    for(var i=0; i< movies.Movies.length; i++) {
-        if((movie_title) == (movies.Movies[i].Title)){
-            movie = movies.Movies[i];
-            movieid = movies.Movies[i].ID;
-            break; 
-        }
-       
+    if(req.query.api.includes('fw')) {
+        fw_movieid = getMovieID(movie_title, movies_fw);
+        options_fw.url = 'http://webjetapitest.azurewebsites.net/api/filmworld/movie/'+fw_movieid ;
+        options = options_fw;
+
     }
-    
-    console.log("Movie id is " +movieid);
+    else 
+    {
+        cw_movieid = getMovieID(movie_title, movies_cw);
+        options_fw.url = 'http://webjetapitest.azurewebsites.net/api/cinemaworld/movie/'+cw_movieid ;
+        options = options_fw
+    }
 
-    //make a http request to api based on specific id 
-    options.url = 'http://webjetapitest.azurewebsites.net/api/cinemaworld/movie/'+movieid ;
+    let single_movie_response = await axios(options);
+    single_movie = single_movie_response.data;
     
-     request(options, function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            single_movie =   JSON.parse(body);   
-            console.log(single_movie.Price);
-            res.render('movie_single', {
-                movies: movies,
-                movie_title: movie_title,
-                movie: movie,
-                single_movie: single_movie
-            })     
-          }
-    }); 
+    res.render('movie_single', {
+        movie_title: movie_title,
+        single_movie: single_movie
+    }) 
 
 };
+ 
 
 //if the page doesnot exist
 exports.notfound =  function(req, res){
     res.send("Error: page not found");
 };
+
+function getMovieID(movie_title, movies_api) {
+    var movie = '';
+    var movieid = '';
+    for(var i=0; i< movies_api.Movies.length; i++) {
+        if((movie_title) == (movies_api.Movies[i].Title)){
+            movie =movies_api.Movies[i];
+            movieid = movies_api.Movies[i].ID;
+            return movieid;
+        }
+       
+    }
+}
